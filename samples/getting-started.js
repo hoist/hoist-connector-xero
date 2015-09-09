47bd9f73d0dd6ae5beb1d49d837173feb903bf6f
Owen Evans <owen@bgeek.net>
2015-09-09 16:56:39 +1200

    fixing getting started

--- samples/getting-started.js
+++ samples/getting-started.js
@@ -1,7 +1,8 @@
 /* Just copy and paste this snippet into your code */
 var _ = require('lodash');
 
-module.exports = function(req, res, done) {
+/* either return a promise or call done, here we're returning a promise */
+module.exports = function(event, done) {
 
   var xero = Hoist.connector("<key>");
 
